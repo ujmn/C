ec37bc2e8c2b7264d6c283ad4384b14628f026d2
ujmn <994189014@qq.com>
2019-05-06 21:27:05 +0800

    update

--- vector/vector/myVector.cpp
+++ vector/vector/myVector.cpp
@@ -34,17 +34,35 @@ myVector::myVector(Iterator first, Iterator last)
 template <class T>
 myVector::myVector(const myVector<T>& v)
 {
+	Reserve(capacity());
+	iterator it = Begin();
+	ConstIterator vit = v.CBegin();
 
+	while (it != v.cend())
+	{
+		*it++ = *vit++;
+	}
+
+	_finish = _start + v.size();
+	_endOfStorage = _start + v.Capacity();
 }
 
 template <class T>
-myVector& myVector::operator =(myVector v)
+myVector& myVector::operator =(myVector<T> v)
 {
+	swap(v);
 
+	return *this;
 }
 
 template <class T>
 myVector::~myVector()
 {
-
+	if (_start != nullptr)
+	{
+		delete[] _start;
+		_start = nullptr;
+		_finish = nullptr;
+		_endOfStorage = nullptr;
+	}
 }
