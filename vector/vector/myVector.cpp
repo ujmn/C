d4b202ce4e3cd1e9f619a668019858bbce6f1b8c
ujmn <994189014@qq.com>
2019-05-07 22:11:37 +0800

    update

--- vector/vector/myVector.cpp
+++ vector/vector/myVector.cpp
@@ -1,6 +1,18 @@
 #include "myVector.h"
 
 template <class T>
+Iterator myVector::Begin() { return _start; }
+
+template <class T>
+Iterator myVector::End() { return _finish; }
+
+template <class T>
+ConstIterator myVector::CBegin() { return _start; }
+
+template <class T>
+ConstIterator myVector::CEnd() { return _finish; }
+
+template <class T>
 myVector::myVector()
 	:_start(nullptr)
 	,_finish(nullptr)
@@ -16,7 +28,7 @@ myVector::myVector(size_t m, const T& value = T())
 	Reserve(n);
 	while (n--)
 	{
-		push_back(value);
+		PushBack(value);
 	}
 }
 
@@ -26,7 +38,7 @@ myVector::myVector(Iterator first, Iterator last)
 	Reserve(last - first);
 	while (first != last)
 	{
-		push_back(*first);
+		PushBack(*first);
 		first++;
 	}
 }
@@ -38,7 +50,7 @@ myVector::myVector(const myVector<T>& v)
 	iterator it = Begin();
 	ConstIterator vit = v.CBegin();
 
-	while (it != v.cend())
+	while (vit != v.CEnd())
 	{
 		*it++ = *vit++;
 	}
@@ -51,7 +63,6 @@ template <class T>
 myVector& myVector::operator =(myVector<T> v)
 {
 	swap(v);
-
 	return *this;
 }
 
@@ -64,5 +75,63 @@ myVector::~myVector()
 		_start = nullptr;
 		_finish = nullptr;
 		_endOfStorage = nullptr;
+	}
+}
+
+template <class T>
+size_t myVector::size() const { return _finish - _start; }
+
+template <class T>
+size_t myVector::max_size() const { return capacity(); }
+
+template <class T>
+void myVector::resize(size_t n, T value = T())
+{
+	size_t size = _start - _finish;
+	if (n < size)
+	{
+		_finish = _start + n;
+		return;
+	}
+
+	if (n > size)
+	{
+		Reserve(n);
+	}
+
+	Iterator it = _finish;
+	_finish = _start + n;
+
+	while (it != _finish)
+	{
+		*it++ = value;
+	}
+}
+
+template <class T>
+size_t myVector::capacity() const { return _endOfStorage - _start; }
+
+template <class T>
+bool myVector::empty() const { return (_start == _finish) ? true : false; }
+
+template <class T>
+void myVector::Reserve(size_t n)
+{
+	if (capacity < n)
+	{
+		size_t size = size();
+		T* temp = new T[n];
+
+		if (_start)
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				temp[i] = _start[i];
+			}
+		}
+
+		_start = temp;
+		_finish = _start + size;
+		_endOfStorage = _start + n;
 	}
 }
--- vector/vector/myVector.h
+++ vector/vector/myVector.h
@@ -6,6 +6,10 @@ class myVector{
 public:
 	typedef T* Iterator;
 	typedef const T* constIterator;
+	Iterator Begin();
+	Iterator End();
+	ConstIterator CBegin();
+	ConstIterator CEnd();
 
 	myVector();
 	myVector(size_t m, const T& value = T());
@@ -13,6 +17,15 @@ public:
 	myVector(const myVector<T>& v);
 	myVector& operator =(myVector v);
 	~myVector();
+	
+
+	//capacity
+	size_t size() const;
+	size_t max_size() const;
+	void resize(size_t n, T c = T());
+	size_t capacity() const;
+	bool empty() const;
+	void Reserve(size_t n);
 
 private:
 	Iterator _start;
